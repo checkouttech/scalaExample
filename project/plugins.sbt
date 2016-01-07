7163218b66f689feb1ce3c46b5e8f352642513bf
Kapil Mahant <kmahant@LA-MAC-KMAHANT2.local>
2016-01-07 11:06:17 -0800

    with corrections

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,15 @@
 
+
 // *** Measure code coverage of unit tests
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.3")
 
 
-// *** To create a rpm using sbt-native-packager
+// *** To create a rpm using sbt-native-packager 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.5")
+
+
+// *** Measure scala style
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
+
+
 
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,29 @@
+/**
+  * A class to represent a ''human being''.
+  *
+  * Specify the `name`, `age`, and `weight` when creating a new `Person`,
+  * then access the fields like this:
+
+  * {{{
+  * val p = Person("Al", 42, 200.0)
+  * p.name
+  * p.age
+  * p.weight
+  * }}}
+  *
+  *
+  * @constructor Create a new person with a `name`, `age`, and `weight`.
+  * @param name The person's name.
+  * @param age The person's age.
+  * @param weight The person's weight.
+  * @author Alvin Alexander
+  * @version 1.0
+  * @todo Add more functionality.
+  * @see See [[http://alvinalexander.com alvinalexander.com]] for more "
+  * information.
+ **/
+
+
 object HelloWorld {
     def main(args: Array[String]) =
         println("Hello World!")
